c3085028586dfa86a9640dc959a228475f3761f3
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2021-11-02 19:25:08 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const sharp = require('sharp');
 
+// generate resized animated gif
 sharp("./homer.gif", { animated: true })
     .resize(100)
     .gif({ pageHeight: 100 })
     .toFile("./homer-resized.gif");
+
+// generate webp, every frame is spread
+sharp("./homer.gif", { animated: true })
+    .resize(100)
+    .webp()
+    .toFile("./homer-resized.webp");
