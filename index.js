b0cac6737669397f5ea1b4a78f177b93eb7e6a74
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2021-11-26 16:10:35 +0900

    add exit status

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const sharp = require('sharp');
 
 // generate resized animated gif
-sharp("./homer.gif", { animated: true })
+Promise.all([
+  sharp("./homer.gif", { animated: true })
     .resize(100)
     .gif({ pageHeight: 100 })
-    .toFile("./homer-resized.gif");
+    .toFile("./homer-resized.gif"),
 
 // generate webp, every frame is spread
-sharp("./homer.gif", { animated: true })
+  sharp("./homer.gif", { animated: true })
     .resize(100)
     .webp()
-    .toFile("./homer-resized.webp");
+    .toFile("./homer-resized.webp"),
 
 // convert heif to jpeg
-sharp("./sample1.heic", { animated: true })
+  sharp("./sample1.heic", { animated: true })
     .resize(100)
     .jpeg()
-    .toFile("./sample1-resized.jpg");
+    .toFile("./sample1-resized.jpg"),
+]).catch(err => {
+  console.error(err)
+  process.exit(1)
+});
